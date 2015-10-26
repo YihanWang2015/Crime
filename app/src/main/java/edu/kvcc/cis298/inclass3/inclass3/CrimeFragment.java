4fe9452566aed950a7a61967bf4500c5e43bdbeb
David Barnes <barnesdavidj@gmail.com>
2015-10-26 14:53:20 -0400

    Add a textchanged listener to the edit text to update the model when the text changes.

--- app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/CrimeFragment.java
+++ app/src/main/java/edu/kvcc/cis298/inclass3/inclass3/CrimeFragment.java
@@ -5,15 +5,19 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 
 public class CrimeFragment extends Fragment {
 
     //Declare a class level variable for a crime
     private Crime mCrime;
+    private EditText mTitleField;
 
     //This method does not do the inflating of the view
     //like the onCreate for an activity does
@@ -30,6 +34,25 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                    ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //This one too.
+            }
+        });
+
         return v;
     }
 }
